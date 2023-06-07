4d2f7bfd84ad8e5d5af4b574d8caa55e6ca6bf77
shayanvazirinareh <135424349+shayanvazirinareh@users.noreply.github.com>
2023-06-07 15:59:09 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily:{
+ display:['bakh']
+    }
+,
+    extend: {},
+  },
+  plugins: [],
+}
+
